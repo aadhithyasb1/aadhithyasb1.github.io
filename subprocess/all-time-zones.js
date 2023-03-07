05281289fa16de7cbfb20b3740660dc7f102da97
aadhithyasb1 <aadhithya.balaji@solitontech.com>
2023-03-07 10:12:40 +0530

    Removed filess

--- subprocess/all-time-zones.js
+++
@@ -1,5 +0,0 @@
-const {
-    allTimeZones,
-  } = require("@aadhithyasb1/time-zone/time-zone");
-
-  process.send(allTimeZones())
--- subprocess/cityname.js
+++
@@ -1,6 +0,0 @@
-const {
-timeForOneCity
-} = require("@aadhithyasb1/time-zone/time-zone");
-process.on("message",(param)=>{
-    process.send(timeForOneCity(param.cityName))
-})
